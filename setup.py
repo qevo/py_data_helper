66b7c693d0dd8d1e463368feb4c581bebb2358c7
Qevo <qevo@qevonics.net>
2015-08-31 03:24:49 -0500

    more repair to setup.py; update docs

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -8,11 +9,9 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-name = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
-packages = [ name.split('_', 1)[1] ]
 
 config = {}
-config["name"]             = name
+config["name"]             = "py_data_helper"
 config["version"]          = "0.0.1"
 config["author"]           = "Qevo"
 config["author_email"]     = "qevo@qevonics.net"
@@ -20,14 +19,14 @@ config["description"]      = ("String search, identification, analysis, and mani
 config["license"]          = "MIT"
 config["keywords"]         = "string hash regex helper"
 config["url"]              = "https://github.com/qevo/" + config["name"]
-config["packages"]         = packages
+config["packages"]         = ["data_helper"]
 config["install_requires"] = []
 config["long_description"] = read('README.md')
 config["classifiers"]      = [
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License"
-    ]
+    "Development Status :: 3 - Alpha",
+    "Topic :: Utilities",
+    "License :: OSI Approved :: MIT License"
+]
 
 if __name__ == '__main__':
     setup(**config)
